4d6a8ab5811e91029b040d4902224a354ec58614
srikk <122169588+py-code314@users.noreply.github.com>
2026-09-19 22:52:56 -0400

    chore(styles): Add and define custom fonts
    Add fonts Cinzel and Quattrocento Sans.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,18 @@ import './App.css'
 
 /* Main App component */
 function App() {
-  return <div className="page">App Component</div>
+  return (
+    <div className="page">
+      App Component
+      <h1>SCRIBLR</h1>
+      <p>
+        Read posts for free! Do you know what you need? You need a blog. Or
+        maybe you don’t, or maybe you already have one, in any case, this
+        project will be a great way to practice and see the benefits of creating
+        an API only backend.
+      </p>
+    </div>
+  )
 }
 
 export default App
