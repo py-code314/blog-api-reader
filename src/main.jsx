4e641275b9c3b804cbb7246286363ee7ab680d5c
srikk <122169588+py-code314@users.noreply.github.com>
2026-09-19 23:21:32 -0400

    chore(router): Add code to import routes into `main.jsx`

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+/* -------------------- Styles -------------------- */
 import './index.css'
-import App from './App.jsx'
+/* -------------------- Components -------------------- */
+import { StrictMode } from 'react'
+import { RouterProvider } from 'react-router'
+/* -------------------- Functions -------------------- */
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter } from 'react-router'
+/* -------------------- Routes -------------------- */
+import routes from './routes/routes.jsx'
+// import App from './App.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+// Create router
+const router = createBrowserRouter(routes)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
